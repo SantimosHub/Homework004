3c8b3a96b3b71b9fadd5cc4c64eb3bf7dff3a135
Sant <stosoleg@gmail.com>
2023-01-18 12:26:51 +0300

    Task029 done

--- Task027/Program.cs
+++ Task027/Program.cs
@@ -1,14 +1,24 @@
 ﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
+// int SumDigit(int number)
+// {
+//     int sum = 0;
+//     while (number / 10 != 0)
+//     {
+//         sum += number % 10;
+//         number = number / 10;
+//     }
+//     sum += number % 10;
+//     return sum;
+// }
+
 int SumDigit(int number)
 {
     int sum = 0;
-    while (number / 10 != 0)
+    for (int i = number; i > 0; i = i / 10)
     {
-        sum += number % 10;
-        number = number / 10;
+        sum += i % 10;
     }
-    sum += number % 10;
     return sum;
 }
 
---
+++ Task029/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+
+
+int[] GetArray()
+{
+    int[] arr = new int[8];
+    for (int i = 0; i < 8; i++)
+    {
+        arr[i] = new Random().Next();
+    }
+
+    return arr;
+}
+
+
+int[] array = GetArray();
+Console.WriteLine($" [ {String.Join(", ", array)} ] ");
